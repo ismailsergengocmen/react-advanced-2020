7a76fc166b864037e523dc6acaee6b754989789f
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-08 14:30:40 +0300

    useState Basic definition

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  /* Uncomment to see useState mechanism. Usestate holds element that will be changed 
+  in their first index and the method that will change the element in their second index*/
 
-export default UseStateBasics;
+  /* console.log(useState('hello world'));
+  const value = useState(1)[0]
+  const handler = useState(1)[1]
+  console.log(value,handler);  */
+
+
+  const [text, setText] = useState('random title')
+  
+  const handleClick = () => {
+    setText(109); //Changes text variable's value to given string 
+  };
+
+  return (
+    <React.Fragment>
+      <h1> {text} </h1>
+      <button className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default UseStateBasics
