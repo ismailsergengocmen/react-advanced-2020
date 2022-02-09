0cad993d2adb1fbcb98a129b7a928b623bdff93f
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-09 11:33:11 +0300

    multiple State Changes

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'hey',
+  })
 
-export default UseStateObject;
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('hey')
+
+  const changeMessage = () => {
+    // This notation enables us to change only message attribute of the person
+    // setPerson({ ...person, message: 'hello world' })
+
+    setMessage('hi')
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{message}</h3>
+      {/*notice that person.message does not change */}
+      <h3>{person.message}</h3> 
+      <button className='btn' onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
+
+
