ec50c9b789f9ccb9a21ac261fe116b2e8dff01a0
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-09 12:02:53 +0300

    useState counter example

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
-};
+  const [value, setValue] = useState(0)
 
-export default UseStateCounter;
+  const reset = () =>{
+    setValue(0)
+  }
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // When only the under line used, value can only be updated once even 
+      // there are many consecutive clicks because it tries to get old value, not the current one
+      // setValue(value + 1);
+
+      //With this combination prevState can be updated multiple times because it gets current value
+      setValue((prevState) => {
+        return prevState + 1; 
+      })
+    }, 2000)
+  }
+
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className='btn' onClick={reset}>
+          reset
+        </button>
+        <button className='btn' onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          increase later
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default UseStateCounter
