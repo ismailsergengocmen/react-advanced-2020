a541a0e1a04c7b4d4da6ce082117e5542221362b
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-10 16:32:29 +0300

    cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setSize] = useState(window.innerWidth)
+  
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
 
-export default UseEffectCleanup;
+  useEffect(() => { //After every render first cleanup function works
+    window.addEventListener('resize',checkSize)
+    /* return () => { //cleanup function
+       console.log('cleanup')
+       window.removeEventListener('resize',checkSize)
+    } */ 
+  },[]) 
+  /* Dependency list works like cleanup function for this particular example. Normally, without cleanup function,
+    in every render there will be new event listener created cumulatitvely. Clean up function prevents that 
+    by being called before. Dependecy list lets useeffect be called only in first render so they reach 
+    to the same conclusion */
+  
+  return (
+    <>
+      <h1>wnidows</h1>
+      <h2>{size} PX </h2>
+    </>
+  )
+}
+
+export default UseEffectCleanup
