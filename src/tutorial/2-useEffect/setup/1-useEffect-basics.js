bfa14d89dd32489040c389e8c8e65132be0b8469
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-10 13:49:31 +0300

    useEffect basics, dependency list, conditional hooks usage

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [value, setValue] = useState(0)
 
-export default UseEffectBasics;
+  //Hooks can not be used in conditional code blocks. So condition added into the hook.
+  useEffect(() => {
+    console.log('call useEffect')
+    if (value >= 1) {
+      document.title = `New Messages(${value})`
+    }
+  }, [value]) // The closed bracket reprensents dependency list. By adding this, when the element inside
+  // closed bracket changes, use effect will be re-rendered. If it's empty, the use effect will only
+  // initially rendered.
+
+  useEffect(() => {
+    console.log('hello world')
+  }, [])
+
+  console.log('render component')
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
+  )
+}
+
+export default UseEffectBasics
