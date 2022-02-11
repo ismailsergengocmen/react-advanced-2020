9e4203ed574424621873c9964491c808414c86b5
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-11 15:08:38 +0300

    short-circuit and ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+  const firstValue = text || 'hello world' //Or operation returns first true or last false
+  const secondValue = text && 'hello world' //And operation returns last true or first false
 
-  return <h2>short circuit</h2>;
-};
+  return (
+    <>
+      {/*<h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' OnClick={() => setIsError(!isError)}>
+        {' '}
+        toggle error
+      </button>
+      {!text && <h1>hello world</h1>}
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>there is an error...</p> : <div> <h2> there is no error...</h2></div>
+      /*If error is true paragraph will be displayed*/} 
+    </>
+  )
+}
 
-export default ShortCircuit;
+export default ShortCircuit
