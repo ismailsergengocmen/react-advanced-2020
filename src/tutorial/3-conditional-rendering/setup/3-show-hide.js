32c1e06601ef8c9525bd3005a5145ba4c64e3622
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-11 17:54:42 +0300

    Show/hide components

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
-};
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      <button className='btn' onClick={() => setShow(!show)}>
+        {' '}
+        show/hide{' '}
+      </button>
+      {show && <Item />}
+    </>
+  )
+}
 
-export default ShowHide;
+const Item = () => {
+  const [size,setSize] = useState(window.innerWidth)
+  
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize',checkSize)
+    return () => {
+      // Cleanup function
+      window.removeEventListener('resize', checkSize)
+    } 
+  },[]) // Dependecy list did not prevent multiple event listener creation because every time
+  // toggle occurs a new event listener created. That's why clean up function needed.
+
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>window</h1> 
+      <h2>size: {size} px</h2>
+    </div>
+  )
+}
+
+export default ShowHide
