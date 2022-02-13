0d093e806beb31e343e0b21cc5fc4aad75e8b57e
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-02-13 18:16:47 +0300

    multiple inputs, dynamic attributes

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -4,27 +4,31 @@ import React, { useState } from 'react';
 // const inputValue = input.value
 // React
 // value, onChange
-// dynamic object keys
+// dynamic object properties 
 
 const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState('');
+  /* const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState(''); */
+  const [person,setPerson] = useState({firstName:'',email:'',age: ''});
   const [people, setPeople] = useState([]);
+
+  const handleChange = (e) =>{
+    const name = e.target.name // get the name attribıte of input
+    const value = e.target.value
+    setPerson({...person,[name]:value}) 
+    //Square brackets lets us to select the attribute that will change
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
-      console.log(person);
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setFirstName('');
-      setEmail('');
-    } else {
-      console.log('empty values');
+    if(person.firstName && person.email && person.age){ //Submit the form if all is entered
+      const newPerson = {...person,id: new Date().getTime().toString()}
+      setPeople([...people,newPerson])
+      setPerson({firstName:"",email:"",age:""})
     }
-  };
+  }
+
   return (
     <>
       <article>
@@ -34,9 +38,9 @@ const ControlledInputs = () => {
             <input
               type='text'
               id='firstName'
-              name='firstName'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              name='firstName' // handleChange get this value and add it to name attribute
+              value={person.firstName}
+              onChange={handleChange}
             />
           </div>
           <div className='form-control'>
@@ -45,24 +49,35 @@ const ControlledInputs = () => {
               type='email'
               id='email'
               name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={person.email}
+              onChange={handleChange}
             />
           </div>
-          <button type='submit'>add person</button>
+          <div className='form-control'>
+            <label htmlFor='age'>Age : </label>
+            <input
+              type='text'
+              id='age'
+              name='age'
+              value={person.age}
+              onChange={handleChange}
+            />
+          </div>
+          <button type='submit' onClick = {handleSubmit}>add person</button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email, age } = person
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <p>{age}</p>
               <p>{email}</p>
             </div>
-          );
+          )
         })}
       </article>
     </>
-  );
+  )
 };
 
 export default ControlledInputs;
